6faf2a8df3bfbc060a7101a09caa84fc2490740c
Fel1X6 <135982142+Fel1X6@users.noreply.github.com>
2026-04-07 16:42:51 +0300

    3

--- ServerSide/cnf.php
+++
@@ -1,8 +0,0 @@
-<?php
-//define('db_ip', '127.0.0.1');
-define('db_ip', '100.93.27.30');
-define('db_port', 3306);
-define('db_usr', 'user2');
-define('db_pwd', '12345');
-define('db_schema', 'budgetbase');
-?>
